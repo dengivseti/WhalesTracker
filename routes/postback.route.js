e1e80f3f717df0d944372a662d87a3220e280432
Dmitrii <dengivseti@list.ru>
2020-08-31 13:59:57 +0300

    feat: add postback

---
+++ routes/postback.route.js
@@ -0,0 +1,28 @@
+const {Router} = require('express')
+const Statistic = require('../models/Statistic')
+
+const router = Router()
+
+router.get('/', async (req, res) => {
+    try{
+        return res.status(500).json('Something went wrong')
+    }catch (e) {
+        res.status(500).json('Something went wrong')
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        if (req.params.id === global.postbackKey && req.query.subid && req.query.payout) {
+            await Statistic.where({subid: req.query.subid}).updateOne({
+                amount: +req.query.payout
+            })
+            return res.json({status: 'ok'})
+        }
+        return res.status(500).json('Something went wrong')
+    } catch (e) {
+        res.status(500).json('Something went wrong')
+    }
+})
+
+module.exports = router
